27a8f7b08eb9fc9c02f34115fa4d650e2796e574
Gavin Hamill <gavin@bashton.com>
2016-01-18 15:49:17 +0000

    Dumb typo. Props to @madandroid for pointing it out

--- manifests/watch.pp
+++ manifests/watch.pp
@@ -25,7 +25,7 @@ define consul_template::watch (
     $frag_name   = "${name}.ctmpl"
     $source_name = "${consul_template::config_dir}/${name}.ctmpl"
   }
-  if $source -= undef {
+  if $source == undef {
     $frag_name   = $name
     $source_name = $name
   } else {  
