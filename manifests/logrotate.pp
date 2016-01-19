878c8920232ddcd129bb991590d04a6f9a27d2b8
Andrey Kanevsky <mrmrh666@gmail.com>
2016-01-19 11:34:53 +0200

    Postrotate command defined for SysV and Systemd by OS
    Postrotate command defined for SysV and Systemd by OS case. Tested on Centos 6, Centos 7, Amazon Linux.

--- manifests/logrotate.pp
+++ manifests/logrotate.pp
@@ -5,11 +5,39 @@ class consul_template::logrotate(
   $logrotate_files,
   $logrotate_on,
   $logrotate_period,
+  $restart_sysv = '/sbin/service consul-template restart',
+  $restart_systemd = '/bin/systemctl restart consul-template.service',
 ) {
   validate_string($logrotate_compress)
   validate_integer($logrotate_files)
   validate_bool($logrotate_on)
   validate_string($logrotate_period)
+  validate_string($restart_sysv)
+  validate_string($restart_systemd)
+
+  case $::osfamily {
+    'RedHat': {
+      case $::operatingsystem {
+        'RedHat', 'CentOS', 'OracleLinux', 'Scientific': {
+          if(versioncmp($::operatingsystemrelease, '7') > 0) {
+            $postrotate_command = $restart_systemd
+          }
+          elsif (versioncmp($::operatingsystemrelease, '7') < 0) {
+            $postrotate_command = $restart_sysv
+          }
+          else {
+            $postrotate_command = $restart_systemd
+          }
+        }
+        'Amazon': {
+          $postrotate_command = $restart_sysv
+        }
+        default: {
+          $postrotate_command = $restart_sysv
+        }
+      }
+    }
+  }
 
   if $logrotate_on {
     file { '/etc/logrotate.d/consul-template':
