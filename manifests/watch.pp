c81836c98be71d1a7c4b30159c8f92ece91fe07a
Ross McFarland <ross@github.com>
2016-06-23 07:00:09 -0700

    Convert consul_template::watch to config_hash

--- manifests/watch.pp
+++ manifests/watch.pp
@@ -4,24 +4,31 @@
 # This is a single instance of a configuration file to watch
 # for changes in Consul and update the local file
 define consul_template::watch (
-  $command,
-  $destination,
-  $source        = undef,
+  $config_hash   = {},
   $template      = undef,
   $template_vars = {},
 ) {
   include consul_template
 
-  if $template == undef and $source == undef {
-    err ('Specify either template or source parameter for consul_template::watch')
+  if $template == undef and $config_hash['source'] == undef {
+    err ('Specify either template parameter or config_hash["source"] for consul_template::watch')
   }
 
-  if $template != undef and $source != undef {
-    err ('Specify either template or source parameter for consul_template::watch - but not both')
+  if $template != undef and $config_hash['source'] != undef {
+    err ('Specify either template parameter or config_hash["source"] for consul_template::watch - but not both')
   }
 
-  if $template != undef {
-    file { "${consul_template::config_dir}/${name}.ctmpl":
+  if $template == undef {
+    # source is specified in config_hash
+    $config_source = {}
+    $frag_name = $config_hash['source']
+  } else {
+    # source is specified as a template
+    $source = "${consul_template::config_dir}/${name}.ctmpl"
+    $config_source = {
+      source => $source,
+    }
+    file { $source:
       ensure  => present,
       owner   => $consul_template::user,
       group   => $consul_template::group,
@@ -30,19 +37,14 @@ define consul_template::watch (
       before  => Concat::Fragment["${name}.ctmpl"],
       notify  => Service['consul-template'],
     }
+    $frag_name = $source
   }
 
-  if $source != undef {
-    $source_name = $source
-    $frag_name   = $source
-  } else {
-    $source_name = "${consul_template::config_dir}/${name}.ctmpl"
-    $frag_name   = "${name}.ctmpl"
-  }
-
+  $config_hash_real = deep_merge($config_hash, $config_source)
+  $content = consul_sorted_json($config_hash_real, $consul::pretty_config, $consul::pretty_config_indent)
   concat::fragment { $frag_name:
     target  => 'consul-template/config.json',
-    content => "template {\n  source = \"${source_name}\"\n  destination = \"${destination}\"\n  command = \"${command}\"\n}\n\n",
+    content => "template ${content}\n\n",
     order   => '10',
     notify  => Service['consul-template']
   }
