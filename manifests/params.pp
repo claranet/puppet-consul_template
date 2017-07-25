33d78444ec275e5219798fcc2a158791a00277e2
Martin Krebs <M.Krebs@t-systems.com>
2017-07-25 13:39:12 +0200

    resolv merge conflict

--- manifests/params.pp
+++ manifests/params.pp
@@ -17,6 +17,9 @@ class consul_template::params {
   $manage_user        = false
   $manage_group       = false
   $config_mode        = '0660'
+  $bin_dir            = '/usr/local/bin'
+  $archive_path       = ''
+  $data_dir           = '/opt/consul-template'
 
   case $::architecture {
     'x86_64', 'amd64': { $arch = 'amd64' }
