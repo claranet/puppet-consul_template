e146b58f171f53f06c3a943fbc019645dd13c364
Craig Watson <craig.watson@claranet.uk>
2018-08-30 09:21:55 +0100

    Function: Fixing Rubocop offences

--- lib/puppet/parser/functions/consul_template_sorted_json.rb
+++ lib/puppet/parser/functions/consul_template_sorted_json.rb
@@ -5,97 +5,93 @@ require 'json'
 
 module JSON
   class << self
-    @@loop = 0
-
     def sorted_generate(obj)
       case obj
-        when NilClass, :undef, Integer, Float, TrueClass, FalseClass, String
-          return simple_generate(obj)
-        when Array
-          arrayRet = []
-          obj.each do |a|
-            arrayRet.push(sorted_generate(a))
-          end
-          return "[" << arrayRet.join(',') << "]";
-        when Hash
-          ret = []
-          obj.keys.sort.each do |k|
-            ret.push(k.to_json << ":" << sorted_generate(obj[k]))
-          end
-          return "{" << ret.join(",") << "}";
-        else
-          raise Exception.new("Unable to handle object of type #{obj.class.name} with value #{obj.inspect}")
+      when NilClass, :undef, Integer, Float, TrueClass, FalseClass, String
+        return simple_generate(obj)
+      when Array
+        array_ret = []
+        obj.each do |a|
+          array_ret.push(sorted_generate(a))
+        end
+        '[' << array_ret.join(',') << ']'
+      when Hash
+        ret = []
+        obj.keys.sort.each do |k|
+          ret.push(k.to_json << ':' << sorted_generate(obj[k]))
+        end
+        '{' << ret.join(',') << '}'
+      else
+        raise Exception, "Unable to handle object of type #{obj.class.name} with value #{obj.inspect}"
       end
     end
 
-    def sorted_pretty_generate(obj, indent_len=4)
-
+    def sorted_pretty_generate(obj, indent_len = 4)
       # Indent length
-      indent = " " * indent_len
+      indent   = ' ' * indent_len
+      loop_num = 0
 
       case obj
-        when NilClass, :undef, Integer, Float, TrueClass, FalseClass, String
-          return simple_generate(obj)
-        when Array
-          arrayRet = []
+      when NilClass, :undef, Integer, Float, TrueClass, FalseClass, String
+        return simple_generate(obj)
+      when Array
+        array_ret = []
 
-          # We need to increase the loop count before #each so the objects inside are indented twice.
-          # When we come out of #each we decrease the loop count so the closing brace lines up properly.
-          #
-          # If you start with @@loop = 1, the count will be as follows
-          #
-          # "start_join": [     <-- @@loop == 1
-          #   "192.168.50.20",  <-- @@loop == 2
-          #   "192.168.50.21",  <-- @@loop == 2
-          #   "192.168.50.22"   <-- @@loop == 2
-          # ] <-- closing brace <-- @@loop == 1
-          #
-          @@loop += 1
-          obj.each do |a|
-            arrayRet.push(sorted_pretty_generate(a, indent_len))
-          end
-          @@loop -= 1
+        # We need to increase the loop count before #each so the objects inside are indented twice.
+        # When we come out of #each we decrease the loop count so the closing brace lines up properly.
+        #
+        # If you start with loop_num = 1, the count will be as follows
+        #
+        # "start_join": [     <-- loop_num == 1
+        #   "192.168.50.20",  <-- loop_num == 2
+        #   "192.168.50.21",  <-- loop_num == 2
+        #   "192.168.50.22"   <-- loop_num == 2
+        # ] <-- closing brace <-- loop_num == 1
+        #
+        loop_num += 1
+        obj.each do |a|
+          array_ret.push(sorted_pretty_generate(a, indent_len))
+        end
+        loop_num -= 1
 
-          return "[\n#{indent * (@@loop + 1)}" << arrayRet.join(",\n#{indent * (@@loop + 1)}") << "\n#{indent * @@loop}]";
+        return "[\n#{indent * (loop_num + 1)}" << array_ret.join(",\n#{indent * (loop_num + 1)}") << "\n#{indent * loop_num}]"
 
-        when Hash
-          ret = []
+      when Hash
+        ret = []
 
-          # This loop works in a similar way to the above
-          @@loop += 1
-          obj.keys.sort.each do |k|
-            ret.push("#{indent * @@loop}" << k.to_json << ": " << sorted_pretty_generate(obj[k], indent_len))
-          end
-          @@loop -= 1
+        # This loop works in a similar way to the above
+        loop_num += 1
+        obj.keys.sort.each do |k|
+          ret.push((indent * loop_num).to_s << k.to_json << ': ' << sorted_pretty_generate(obj[k], indent_len))
+        end
+        loop_num -= 1
 
-          return "{\n" << ret.join(",\n") << "\n#{indent * @@loop}}";
-        else
-          raise Exception.new("Unable to handle object of type #{obj.class.name} with value #{obj.inspect}")
+        return "{\n" << ret.join(",\n") << "\n#{indent * loop_num}}"
+      else
+        raise Exception, "Unable to handle object of type #{obj.class.name} with value #{obj.inspect}"
       end
-
     end # end def
+
     private
+
     # simplify jsonification of standard types
     def simple_generate(obj)
       case obj
-        when NilClass, :undef
-          'null'
-        when Integer, Float, TrueClass, FalseClass
-          "#{obj}"
-        else
-          # Should be a string
-          # keep string integers unquoted
-          (obj =~ /\A[-]?(0|[1-9]\d*)\z/) ? obj : obj.to_json
+      when NilClass, :undef
+        'null'
+      when Integer, Float, TrueClass, FalseClass
+        obj.to_s
+      else
+        # Should be a string
+        # keep string integers unquoted
+        (obj =~ %r{\A[-]?(0|[1-9]\d*)\z}) ? obj : obj.to_json
       end
     end
-
   end # end class
-
 end # end module
 
-
 module Puppet::Parser::Functions
-  newfunction(:consul_template_sorted_json, :type => :rvalue, :doc => <<-EOS
+  newfunction(:consul_template_sorted_json, type: :rvalue, doc: <<-EOS
 This function takes unsorted hash and outputs JSON object making sure the keys are sorted.
 Optionally you can pass 2 additional parameters, pretty generate and indent length.
 
@@ -152,16 +148,13 @@ Optionally you can pass 2 additional parameters, pretty generate and indent leng
     }
 
     EOS
-  ) do |args|
+             ) do |args|
 
     unsorted_hash = args[0]      || {}
     pretty        = args[1]      || false
     indent_len    = args[2].to_i || 4
 
-    if pretty
-      return JSON.sorted_pretty_generate(unsorted_hash, indent_len) << "\n"
-    else
-      return JSON.sorted_generate(unsorted_hash)
-    end
+    return JSON.sorted_pretty_generate(unsorted_hash, indent_len) << "\n" if pretty
+    JSON.sorted_generate(unsorted_hash)
   end
 end
